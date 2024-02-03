4b69d52cb084ec17893a385050a8b2a2c5f77a2b
Tim <timbcodes@gmail.com>
2024-02-02 21:16:33 -0600

    Comitting what I have before I take a break

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,9 @@
 <template>
   <router-view />
 </template>
+<style lang="scss" scoped>
+template {
+  height: 100vh;
+  width: 100%;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import MainHome from "@/views/MainHome";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "MainHome",
+    component: MainHome,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
